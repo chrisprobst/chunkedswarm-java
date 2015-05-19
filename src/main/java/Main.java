0d5ed5efd94202e6ee2fbad2eb9799230a95057d
Christopher Probst <christopher.probst@uni-duesseldorf.de>
2015-05-19 12:03:32 +0200

    Init commit - add netty

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author Christopher Probst <christopher.probst@hhu.de>
+ * @version 1.0, 19.05.15
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+}
